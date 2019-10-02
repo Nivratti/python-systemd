0b6d72248d55759af6ebf5c420a50a58ac977608
Nivratti Boyane <boyanenivratti@gmail.com>
2019-10-02 15:43:14 +0530

    Python demo created
    Sample python demo to show dameon process

---
+++ python-demo.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python -u
+import time
+
+def main():
+    print("HI")
+    
+if __name__ == "__main__":
+    print('Starting up ...')
+    time.sleep(10)
+    print('Startup complete')
+    main()
+    print('Execution completed')
+    
